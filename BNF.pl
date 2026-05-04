c54276f8014b127f7c649b0e125ac4da50c97807
bugsb601 <swcastillo601@gmail.com>
2026-05-03 22:47:34 -0600

    Se agrega la logica final y se hicieron correcciones en el codigo

--- BNF.pl
+++ BNF.pl
@@ -40,51 +40,85 @@ articulo --> [unas].
 /*Expresiones que se usan para indicar que algo gusta o no*/
 /*positivo*/
 expresion(positivo) --> [amo].
+expresion(positivo) --> [adoro].
 expresion(positivo) --> [me, gusta].
-expresion(positivo) --> [me, encanta]. 
-expresion(positivo) --> [me, fascina]. 
-expresion(positivo) --> [me, apasiona]. 
-expresion(positivo) --> [me, interesa]. 
-expresion(positivo) --> [me, atrae]. 
-expresion(positivo) --> [me, agrada]. 
-expresion(positivo) --> [disfruto]. 
-
-/*negativo*/
+expresion(positivo) --> [me, gustan].           
+expresion(positivo) --> [me, encanta].
+expresion(positivo) --> [me, encantan].
+expresion(positivo) --> [me, fascina].
+expresion(positivo) --> [me, apasiona].
+expresion(positivo) --> [me, interesa].
+expresion(positivo) --> [me, interesan].
+expresion(positivo) --> [me, atrae].
+expresion(positivo) --> [me, agrada].
+expresion(positivo) --> [me, agradan].
+expresion(positivo) --> [disfruto].
+expresion(positivo) --> [disfruto, de].
+expresion(positivo) --> [disfruto, mucho, de].
+expresion(positivo) --> [me, entusiasma].
+expresion(positivo) --> [me, llama, la, atencion].
+expresion(positivo) --> [soy, muy, habil, con].
+expresion(positivo) --> [soy, bueno, en].
+expresion(positivo) --> [me, intereso, por].
+expresion(positivo) --> [tengo, interes, por].
+expresion(positivo) --> [prefiero].            
+expresion(positivo) --> [me, gusta, mucho].
+expresion(positivo) --> [me, encanta, mucho].
+expresion(positivo) --> [me, intereza].        
+ 
 expresion(negativo) --> [odio].
+expresion(negativo) --> [detesto].
 expresion(negativo) --> [no, me, gusta].
-expresion(negativo) --> [no, me, encanta]. 
-expresion(negativo) --> [no, me, fascina]. 
-expresion(negativo) --> [no, me, apasiona]. 
-expresion(negativo) --> [no, me, interesa]. 
-expresion(negativo) --> [no, me, atrae]. 
-expresion(negativo) --> [no, me, agrada]. 
-expresion(negativo) --> [me, aburre]. 
-
+expresion(negativo) --> [no, me, gustan].
+expresion(negativo) --> [no, me, encanta].
+expresion(negativo) --> [no, me, encantan].
+expresion(negativo) --> [no, me, fascina].
+expresion(negativo) --> [no, me, apasiona].
+expresion(negativo) --> [no, me, interesa].
+expresion(negativo) --> [no, me, interesan].
+expresion(negativo) --> [no, me, atrae].
+expresion(negativo) --> [no, me, agrada].
+expresion(negativo) --> [me, aburre].
+expresion(negativo) --> [me, aburren].
+expresion(negativo) --> [me, desagrada].
+expresion(negativo) --> [no, disfruto].
+expresion(negativo) --> [no, podria].
+expresion(negativo) --> [jamas, podria].
+expresion(negativo) --> [no, me, gusta, mucho].
+expresion(negativo) --> [no, me, gustan, mucho].
+ 
 
 /*Los temas y con sus posibles variaciones*/
 tema(matematicas) --> [matematicas].
 tema(matematicas) --> [numeros].
+tema(matematicas) --> [calculo].
+tema(matematicas) --> [calculos].
 
 tema(tecnologia) --> [tecnologia].
 tema(tecnologia) --> [computadoras].
 tema(tecnologia) --> [software].
 tema(tecnologia) --> [hardware].
 tema(tecnologia) --> [computacion].
+tema(tecnologia) --> [programacion].
 
 tema(problemas) --> [problemas].
 tema(problemas) --> [retos].
 tema(problemas) --> [obstaculos].
 tema(problemas) --> [conflictos].
 tema(problemas) --> [contratiempos].
+tema(problemas) --> [desafios].
 
 tema(personas) --> [personas].
 tema(personas) --> [gente].
 tema(personas) --> [individuos].
 tema(personas) --> [sujetos].
+tema(personas) --> [equipo].
+tema(personas) --> [otros].
 
 tema(escucha) --> [escucha].
 tema(escucha) --> [escuchar].
 tema(escucha) --> [oir].
+tema(escucha) --> [hablar].
 
 tema(idiomas) --> [idiomas].
 tema(idiomas) --> [lenguas].
@@ -93,35 +127,55 @@ tema(idiomas) --> [ingles].
 
 tema(comunicacion) --> [comunicacion].
 tema(comunicacion) --> [conversacion].
+tema(comunicacion) --> [expresion].
 
 tema(organizacion) --> [organizacion].
+tema(organizacion) --> [organizar].
 
 tema(ensenanza) --> [ensenanza].
 tema(ensenanza) --> [ensenar].
+tema(ensenanza) --> [docencia].
+tema(ensenanza) --> [clases].
 
 tema(paciencia) --> [paciencia].
+tema(paciencia) --> [calma].
+tema(paciencia) --> [tolerancia].
 
 tema(biologia) --> [biologia].
+tema(biologia) --> [ciencias].
+tema(biologia) --> [naturaleza].
 
 tema(investigacion) --> [investigacion].
 tema(investigacion) --> [investigar].
+tema(investigacion) --> [explorar].
+tema(investigacion) --> [indagar].
 
 tema(laboratorio) --> [laboratorio].
+tema(laboratorio) --> [laboratorios].
+tema(laboratorio) --> [quimica].
+tema(laboratorio) --> [experimentos].
 
 tema(creatividad) --> [creatividad].
 tema(creatividad) --> [imaginacion].
 tema(creatividad) --> [ingenio].
+tema(creatividad) --> [arte].
 
 tema(debatir) --> [debatir].
 tema(debatir) --> [discutir].
 tema(debatir) --> [disputar].
+tema(debatir) --> [argumentar].
+tema(debatir) --> [debate].
 
 tema(analisis) --> [analisis].
 tema(analisis) --> [analizar].
 tema(analisis) --> [examinar].
+tema(analisis) --> [evaluar].
 
 tema(justicia) --> [justicia].
 tema(justicia) --> [leyes].
+tema(justicia) --> [derecho].
+tema(justicia) --> [legal].
+tema(justicia) --> [ley].
 
 tema(ayudar) --> [ayudar].
 tema(ayudar) --> [asistir].
@@ -130,30 +184,51 @@ tema(ayudar) --> [cooperar].
 tema(ayudar) --> [apoyar].
 tema(ayudar) --> [socorrer].
 tema(ayudar) --> [auxiliar].
+tema(ayudar) --> [servir].
 
 tema(fluidos) --> [fluidos].
 tema(fluidos) --> [sangre].
 tema(fluidos) --> [vomito].
+tema(fluidos) --> [heridas].
 
 tema(estres) --> [estres].
+tema(estres) --> [presion].
+tema(estres) --> [ansiedad].
 
 tema(aislamiento) --> [aislamiento].
+tema(aislamiento) --> [soledad].
+tema(aislamiento) --> [aislado].
 
 tema(liderazgo) --> [liderazgo].
 tema(liderazgo) --> [lider].
+tema(liderazgo) --> [dirigir].
+tema(liderazgo) --> [mandar].
+tema(liderazgo) --> [liderar].
 
 tema(oficina) --> [oficina].
 tema(oficina) --> [sentado].
+tema(oficina) --> [escritorio].
 
 tema(desorden) --> [desorden].
+tema(desorden) --> [caos].
 
 tema(improvisacion) --> [improvisacion].
+tema(improvisacion) --> [improvisar].
+tema(improvisacion) --> [espontaneo].
 
 tema(empatia) --> [empatia].
+tema(empatia) --> [comprender].
+tema(empatia) --> [sentir].
 
 tema(manual) --> [manual].
+tema(manual) --> [fisico].
+tema(manual) --> [manos].
+tema(manual) --> [artesanal].
 
 tema(diseno) --> [diseno].
+tema(diseno) --> [disenar].
+tema(diseno) --> [grafico].
+tema(diseno) --> [visual].
 
 
 /*Palabras de inicio distintas de una oracion normal*/
---
+++ Logic.pl
@@ -0,0 +1,151 @@
+/* Logic.pl */
+
+/* Consultar las bases de datos necesarias */
+:- consult('BD.pl').
+:- consult('BNF.pl').
+
+/* Dinamicamente almacenar preferencias de los usuarios */
+:- dynamic gusta/1.
+:- dynamic nogusta/1.
+
+
+preferencia(positivo, Tema) :-
+    (gusta(Tema) -> true ; assertz(gusta(Tema))).
+
+preferencia(negativo, Tema) :-
+    (nogusta(Tema) -> true ; assertz(nogusta(Tema))).
+
+limpiap :-
+    retractall(gusta(_)),
+    retractall(nogusta(_)).
+
+/*Sistema Experto*/
+cumple(Carrera) :-
+    profesion(Carrera, Fortalezas, Antagonias),
+    forall(member(X, Fortalezas), gusta(X)),
+    \+ (member(Y, Antagonias), gusta(Y)),
+    \+ (member(Z, Fortalezas), nogusta(Z)).
+
+recomendar(Carrera) :-
+    cumple(Carrera), !.
+recomendar(ninguna).
+
+
+texto_a_tokens(Texto, Tokens) :-
+    string_lower(Texto, Lower),
+    split_string(Lower, " \t", ".,!?;:()", Partes),
+    exclude(=(""), Partes, Partes2),
+    maplist(atom_string, Tokens, Partes2).
+
+parsear(Tokens, Sentimiento, Tema) :-
+    phrase(oracion(Sentimiento, Tema), Tokens, _), !.
+
+
+respuesta_no(Tokens) :-
+    member(N, [nunca, jamas, negativo, tampoco,
+               nada, imposible, dificilmente, ninguno, ninguna]),
+    member(N, Tokens), !.
+
+respuesta_si(Tokens) :-
+    \+ respuesta_no(Tokens),
+    member(P, [claro, correcto, exacto, afirmativo,
+               definitivamente, efectivamente, mucho, bastante,
+               obvio, seguro, ok]),
+    member(P, Tokens), !.
+
+/*Preguntas guiadas tipo Akinator*/
+pregunta_guiada(matematicas, "Te gustan las matematicas o los numeros").
+pregunta_guiada(tecnologia, "Te interesa la tecnologia o las computadoras").
+pregunta_guiada(personas, "Disfrutas trabajar con personas o en equipo").
+pregunta_guiada(problemas, "Te gusta resolver problemas o retos complejos").
+pregunta_guiada(escucha, "Cuando conversas, prefieres escuchar o hablar").
+pregunta_guiada(empatia, "Eres empatico, comprendes facilmente los sentimientos ajenos").
+pregunta_guiada(biologia, "Te atrae la biologia o las ciencias naturales").
+pregunta_guiada(investigacion, "Disfrutas investigar o explorar temas a fondo").
+pregunta_guiada(laboratorio, "Te interesa trabajar en laboratorio o con experimentos").
+pregunta_guiada(creatividad, "Eres creativo, te gusta el diseno o el arte").
+pregunta_guiada(comunicacion, "Te gusta comunicarte o expresarte con facilidad").
+pregunta_guiada(idiomas, "Te gustan los idiomas o las lenguas extranjeras").
+pregunta_guiada(organizacion, "Te consideras organizado y planificador").
+pregunta_guiada(liderazgo, "Te gusta liderar o tomar decisiones de mando").
+pregunta_guiada(debatir, "Disfrutas debatir, argumentar o discutir ideas").
+pregunta_guiada(analisis, "Te gusta analizar situaciones o datos con detalle").
+pregunta_guiada(justicia, "Te interesa la justicia, las leyes o lo legal").
+pregunta_guiada(ayudar, "Te motiva ayudar a los demas").
+pregunta_guiada(ensenanza, "Te gusta ensenar o transmitir conocimiento a otros").
+pregunta_guiada(paciencia, "Eres paciente y tolerante con los demas").
+
+tema_cubierto(Tema) :- gusta(Tema).
+tema_cubierto(Tema) :- nogusta(Tema).
+
+siguiente_pregunta(Tema, Texto) :-
+    pregunta_guiada(Tema, Texto),
+    \+ tema_cubierto(Tema).
+
+
+iniciar :-
+    limpiap,
+    writeln("OrientadorCE: Hola, se que la tarea de buscar una carrera es dificil."),
+    writeln("              Estamos aqui para ayudarte! Dime que te gusta."),
+    nl,
+    bucle_libre.
+
+bucle_libre :-
+    write("Usuario: "),
+    read_line_to_string(user_input, LineStr),
+    string_to_atom(LineStr, Linea),
+    procesar_libre(Linea).
+
+procesar_libre(salir) :-
+    writeln("OrientadorCE: Hasta luego! Espero haberte ayudado.").
+
+procesar_libre(Linea) :-
+    Linea \= salir,
+    texto_a_tokens(Linea, Tokens),
+    (   parsear(Tokens, Sentimiento, Tema)
+    ->  preferencia(Sentimiento, Tema),
+        responder_acuse(Sentimiento)
+    ;   writeln("OrientadorCE: Interesante, cuentame mas sobre tus gustos.")
+    ),
+    continuar_o_recomendar.
+
+responder_acuse(positivo) :-
+    writeln("OrientadorCE: Excelente, tome nota de eso!").
+responder_acuse(negativo) :-
+    writeln("OrientadorCE: Entendido, lo tendre en cuenta.").
+
+continuar_o_recomendar :-
+    recomendar(Carrera),
+    (   Carrera \= ninguna
+    ->  format("OrientadorCE: Dadas tus preferencias, te recomendaria estudiar ~w.~n", [Carrera])
+    ;   (   siguiente_pregunta(Tema, TextoPregunta)
+        ->  format("OrientadorCE: ~w~n", [TextoPregunta]),
+            bucle_guiado(Tema)
+        ;   writeln("OrientadorCE: No encontre una carrera exacta, por favor intenta nuevamente."),
+            writeln("              Escribe iniciar. en la consola para reiniciar.")
+        )
+    ).
+
+bucle_guiado(TemaEsperado) :-
+    write("Usuario: "),
+    read_line_to_string(user_input, LineStr),
+    string_to_atom(LineStr, Linea),
+    (   Linea = salir
+    ->  writeln("OrientadorCE: Hasta luego!")
+    ;   texto_a_tokens(Linea, Tokens),
+        (   parsear(Tokens, Sentimiento, _)
+        ->  preferencia(Sentimiento, TemaEsperado),
+            responder_acuse(Sentimiento),
+            continuar_o_recomendar
+        ;   respuesta_no(Tokens)
+        ->  preferencia(negativo, TemaEsperado),
+            responder_acuse(negativo),
+            continuar_o_recomendar
+        ;   respuesta_si(Tokens)
+        ->  preferencia(positivo, TemaEsperado),
+            responder_acuse(positivo),
+            continuar_o_recomendar
+        ;   writeln("OrientadorCE: No entendi esa respuesta, me puedes repetir?"),
+            bucle_guiado(TemaEsperado)
+        )
+    ).
