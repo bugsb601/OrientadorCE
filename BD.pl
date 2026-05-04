4a0f0924f2552d902acd4de94988050e625871b4
bugsb601 <swcastillo601@gmail.com>
2026-05-03 21:13:56 -0600

    Base de datos

---
+++ BD.pl
@@ -0,0 +1,14 @@
+/*Base de Datos*/
+
+
+/*10 profesiones con sus fortalezas y antagonias*/
+profesion(ingenieria_computadores, [matematicas, tecnologia, problemas], [personas, manual]).
+profesion(psicologia, [personas, escucha, empatia], [matematicas, tecnologia]).
+profesion(turismo, [personas, idiomas, comunicacion], [oficina, aislamiento]).
+profesion(administracion, [matematicas, organizacion, liderazgo], [desorden, improvisacion]).
+profesion(docencia, [ensenanza, personas, paciencia], [aislamiento]).
+profesion(biotecnologia, [biologia, investigacion, laboratorio], [personas]).
+profesion(diseno_publicitario, [creatividad, comunicacion, diseno], [matematicas, aislamiento]).
+profesion(derecho, [debatir, analisis, justicia], [matematicas, liderazgo]).
+profesion(criminologia, [justicia, investigacion, analisis], [fluidos, personas]).
+profesion(medicina, [ayudar, personas, biologia], [fluidos, estres]).
